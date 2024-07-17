ec1d4f7b76181d63a5ff27717bb786a769a6c5a3
konstantine168 <basapapava@gmail.com>
2024-07-17 19:43:14 +0400

    commit

---
+++ just a test 9.py
@@ -0,0 +1,18 @@
+#BONUS lesson 
+#lets learn MORE  from the turtle import
+#lets make deco for youre moving cursor!
+#first of all design
+from turtle import *
+color ("purple")
+#now the shape!
+shape ("turtle")
+shape ("cube")
+shape ("triangle")
+shape ("circle")
+
+
+
+
+
+exitonclick()
+
