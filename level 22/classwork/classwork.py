6b8db52e9c910c8e1a901d5e2f014b7c4a20eeb7
konstantine168 <basapapava@gmail.com>
2024-09-06 17:41:02 +0400

    commit

--- level 22/classwork/classwork.py
+++ level 22/classwork/classwork.py
@@ -1,5 +1,5 @@
 # classwork:
-# replace every name in the list with "lobio"
+# replace every item in the list with "lobio"
 favorite_foods_list = ["watermelon","apple","მჭადი და ყველი","სალათი(+ vitamins)"]
 favorite_foods_list[0] = "lobio"
 favorite_foods_list[1] = "lobio"
