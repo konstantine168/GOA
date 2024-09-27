e5de2377707dfc3a444a319231f68e9b141d9dcb
konstantine168 <basapapava@gmail.com>
2024-09-27 17:05:47 +0400

    commit

---
+++ level 29/classwork/homework/homework.py
@@ -0,0 +1,20 @@
+# 1)
+def function(print_hellow):
+    print_hello = []
+    print_hello.append("გამარჯობა, მეგობრებო!")
+    print(print_hello)
+# 2)
+def function(birthday_name):
+    birthday_name = input("enter your name:")
+    print("დაბადების დღეს გილოცავ!" + " " + birthday_name)
+# 3)
+def function(favorite_color):
+    fav_color = input("input your favorite color:")
+    print("შენი საყვარელი ფერია" + " " + fav_color)
+# 5)
+def function(len_text):
+    len_text = input("enter text:")
+    print(len(len_text))
+# 7)
+bruh = ["hi"]
+bruh.print_hellow()
